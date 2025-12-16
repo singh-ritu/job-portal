de5da18d70f2c0379379a302014e915ff544e456
Ritu Singh <ritusingh@Ritus-MacBook-Air.local>
2025-12-16 20:54:21 +0530

    feat: Fetch Jobs with filters & pagination

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,50 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import {Job} from "@/types/job";
+
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "";
+
+if (!API_BASE_URL) {
+  throw new Error("API base URL is not defined");
+}
+
+interface GetJobsParams{
+  page?: number;
+  limit?: number;
+  keyword?: string;
+  location?: string;
+  jobType?: string;
+}
+
+// FETCH JOBS WITH FILTERS AND PAGINATION
+export async function getPublicJobs(params: GetJobsParams) {
+
+  const {
+    page = 1,
+    limit = 10,
+    keyword,
+    location,
+    jobType,
+  } = params;
+
+  const query = new URLSearchParams();
+  
+  query.append("page", page.toString());
+  query.append("limit", limit.toString());
+
+  if (keyword) query.append("keyword", keyword);
+  if (location) query.append("location", location);
+  if (jobType) query.append("jobType", jobType);
+
+  const res = await fetch(`${API_BASE_URL}/api/jobs?${query.toString()}`)
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch jobs");
+  }
+  return res.json();
+}
+
+
+// REGISTER USER
 export async function registerUser(data: {
   name: string;
   email: string;
@@ -20,6 +66,7 @@ export async function registerUser(data: {
   return res.json();
 }
 
+// LOGIN USER
 export async function loginUser(data: {
   email: string;
   password: string;
